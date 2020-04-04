fa846d44a615306c43233837ccd57f7bc0160073
Zaytt <ivan_chavez_e@hotmail.com>
2020-04-03 19:03:24 -0600

    Created Bootcamp model

---
+++ controllers/bootcamps.js
@@ -0,0 +1,44 @@
+/**
+ * @desc  Get all bootcamps
+ * @route GET /api/v1/bootcamps
+ * @access Public
+ */
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'show all bootcamps' });
+};
+
+/**
+ * @desc  Get a single bootcamp
+ * @route GET /api/v1/bootcamps/:id
+ * @access Public
+ */
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `show bootcamp with id ${req.params.id}` });
+};
+
+/**
+ * @desc  Create a single bootcamp
+ * @route POST /api/v1/bootcamps
+ * @access Private
+ */
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: 'create new bootcamp' });
+};
+
+/**
+ * @desc  UPdate a single bootcamp
+ * @route PUT /api/v1/bootcamps/:id
+ * @access Private
+ */
+exports.updateBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `update bootcamp ${req.params.id}` });
+};
+
+/**
+ * @desc  Delete a bootcamp
+ * @route DELETE /api/v1/bootcamps/:id
+ * @access Private
+ */
+exports.deleteBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `delete bootcamp ${req.params.id}` });
+};
